0a8e20eb218fc3bcba688093a6df4f31925a93f7
mijekassandra20 <kassandra.mije@supportzebra.com>
2022-11-07 13:11:27 +0800

    Assignment 3: w
    eek4

--- sandboxJest/sandbox2.js
+++ sandboxJest/sandbox2.js
@@ -1,81 +1,71 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const twoSum = (nums, target) => {
-    for (let i = 0; i < nums.length; i++) { // 3
-        for(let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-            return [i, j]
+    for (let i = 0; i < nums.length; i++) { // 0, 
+        for (let j = i + 1; j < nums.length; j++) { // 1
+            if (nums[i] + nums[j] === target) { // nums[0] + nums[1] === target
+                return [i, j];
             }
         }
     }
 
     return;
-}
+};
 
 const fetchProfile = async () => {
     let result;
 
     try {
-        result = await axios.get('https://jsonplaceholder.typicode.com/users/2')
+        result = await axios.get(
+            "https://jsonplaceholder.typicode.com/users/2"
+        );
     } catch (err) {
-        throw new Error('Error fetching for profile')
+        throw new Error("Error fetching for profile");
     }
 
-        return result;
-}
+    return result;
+};
 
 // e.g. arr = [1, 2, 4, 5], numberToRemove = 4
 const removeNumberFromArray = (arr, numberToRemove) => {
     if (!arr.length) {
-        throw new Error('Missing array');
-
+        throw new Error("Missing array");
     } else if (arr.includes(numberToRemove)) {
         const index = arr.indexOf(numberToRemove);
         arr.splice(index, 1);
         return arr;
-
     } else {
-        throw new Error(`Array does not include number ${numberToRemove}`)
-
+        throw new Error(`Array does not include number ${numberToRemove}`);
     }
-}
+};
 
-// e.g. obj = {}, property = 'name', value = ‘tony
+// e.g. obj = {}, property = 'name', value = 'tony'
 const addNewProperty = (obj, property, value) => {
-
     if (!property && !value) {
-        throw new Error('Missing both property and value');
-
+        throw new Error("Missing both property and value");
     } else {
         obj[property] = value;
-        return obj
-
+        return obj;
     }
-}
+};
 
 // e.g. arr = [3, 1, 2, 5]
 const sortArray = (arr) => {
-
     if (!arr) {
-        throw new Error('Missing array');
-
+        throw new Error("Missing array");
     } else {
-        return arr.sort()
-
+        return arr.sort();
     }
-}
+};
 
 // e.g. words = ['cat', 'dog', 'pineapple']
 const upperCaseWords = (words) => {
-
-    if (!arr) {
-        throw new Error('Missing words array');
-
+    if (!words) {
+        throw new Error("Missing words array");
     } else {
-        return words.map(word => word.toUpperCase());
-
+        return words.map((word) => word.toUpperCase());
     }
-}
+};
 
 module.exports = {
     twoSum,
@@ -83,5 +73,5 @@ module.exports = {
     removeNumberFromArray,
     addNewProperty,
     sortArray,
-    upperCaseWords
-}
+    upperCaseWords,
+};
--- sandboxJest/sandbox2.test.js
+++ sandboxJest/sandbox2.test.js
@@ -4,44 +4,129 @@ const {
     removeNumberFromArray,
     addNewProperty,
     sortArray,
-    upperCaseWords
+    upperCaseWords,
+} = require("./sandbox2");
 
-} = require('./sandbox2')
-
-const axios = require('axios')
+const axios = require("axios");
 
 // ##########################################################################################
 
-describe('twoSum', () => {
+describe('tests twoSum function', () => {
 
     test('should return an array of the index position of two numbers that is equal to the target', () => {
-        const numbers = [1, 2, 3]
-        const target = 5
+        const numbers = [1, 2, 3];
+        const target = 5;
 
-        expect(twoSum(numbers, target)).toEqual([1,2]);
-    })
+        expect(twoSum(numbers, target)).toEqual([1, 2]);
+    });
 
     test('should return undefined, if there are no pair of numbers that sums up to the target value', () => {
-        const numbers = [1, 2, 3]
-        const target = 9
-        
+        const numbers = [1, 2, 3];
+        const target = 9;
+
         expect(twoSum([], 5)).toBeUndefined();
         expect(twoSum(numbers, target)).toBeFalsy();
+    });
+});
 
+// ##########################################################################################
+
+describe('tests fetchProfile function', () => {
+
+    test('should return an email of `Shanna@melissa.tv` ', async () => {
+        // fetch data
+        const fetchData = await fetchProfile();
+
+        expect(fetchData.data.email).toEqual('Shanna@melissa.tv');
+    });
+
+    test('should return the city of `Wisokyburgh` ', async () => {
+        const fetchData = await fetchProfile();
+
+        expect(fetchData.data.address.city).toEqual('Wisokyburgh');
+    });
+
+    test('should return a company name of `Deckow-Crist` ', async () => {
+        const fetchData = await fetchProfile();
+
+        expect(fetchData.data.company.name).toEqual('Deckow-Crist');
+    });
+
+    test('should return false if it DOES NOT have a zipcode of `90388-2220` ', async () => {
+        const fetchData = await fetchProfile();
+
+        expect(fetchData.data.address.zipcode).not.toContain('90388-2220');
+    });
+});
+
+// ##########################################################################################
+
+describe('tests removeNumberFromArray function', () => {
+
+    test('should return an error that says `Missing Array!`, if no array was passed', () => {
+        expect(() => removeNumberFromArray([], 0)).toThrow(Error);
+    });
+
+    test('should return a new array excluding the number that was removed', () => {
+        numbers = [10, 20, 30, 40, 50];
+        removeNumber = 30;
+
+        expect(removeNumberFromArray(numbers, removeNumber)).toEqual([10, 20, 40, 50]);
+    });
+
+    test('should return an error, if a number that was supposed to be removed did not exists in the array', () => {
+        numbers = [10, 20, 30, 40, 50];
+        removeNumber = 60;
+
+        expect(() => removeNumberFromArray(numbers, removeNumber).toThrow(Error)
+        );
+    });
+});
+
+// ##########################################################################################
+
+describe('tests addNewProperty function', () => {
+
+    test('should return an error, if property and value are undefined', () => {
+        expect(() => addNewProperty({}, '', '').toThrow(Error));
+    })
+
+    test('should return the new key-value pair that was added in your object', () => {
+
+        object = { firstName: 'Tony' }
+        property = 'lastName'
+        value = 'Kim'
+
+        expect(addNewProperty(object, property, value)).toEqual({ firstName: 'Tony', lastName: 'Kim' })
+
+    })
+});
+
+// ##########################################################################################
+
+describe('test sortArray function', () => {
+
+    test('should return an error message, if no array was passed', () => {
+        expect(() => sortArray()).toThrow(Error)
+    })
+
+    test('should return the sorted array', () => {
+        numbers = [8, 2, 6, 4]
+
+        expect(sortArray(numbers)).toEqual([2, 4, 6, 8])
     })
 
 })
 
 // ##########################################################################################
 
-describe('fetchProfile', () => {
-    
-    test('', () => {
-        // fetch data
-        
+describe('tests upperCaseWords function', () => {
+
+    test('should return an error message, if no error message was passed', () => {
+        expect(() => upperCaseWords().toThrow(Error))
     })
 
-
+    test('should return the capitalized version of ther words passed in an array', () => {
+        expect(upperCaseWords(['cat','dog'])).toEqual(['CAT','DOG'])
+    })
 })
-
-
